e868e0a8a413c4879f1b4c6c0316f5d9d8a86c43
Hannah Lee <013hannah.a.lee@gmail.com>
2021-03-07 01:10:49 -0600

    planner will now change colors based on the hour

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,28 @@
 var displayDate = document.getElementById("currentDay");
 var todaysDate = moment().format('dddd MMMM DD, YYYY');
 var saveBtn = document.getElementById("save");
+var hours = new Date().getHours();
+var hour = JSON.stringify(hours);
 
 displayDate.innerHTML = todaysDate;
+console.log(hours + " hour");
 
-// function timeStampColor () {
-//     if (moment())
-// };
+//changes the color of the planner by the time
+$('.input-group').each(function () {
+    console.log($('.input-group-text', this).text() );
+    var currentTime = $('.input-group-text', this).text();
+    var timeStampColor = $('.color', this);
+
+    if (currentTime === hour) {
+        console.log("present");
+        timeStampColor.addClass('present');
+    } else if (currentTime < hour) {
+        console.log("past")
+        timeStampColor.addClass('past');
+    } else {
+        console.log("future")
+        timeStampColor.addClass('future');
+    }});
+
+   
+ 
