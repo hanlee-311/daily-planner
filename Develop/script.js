7789bf41809e567a01ce5810fde51e0e02d59d29
Hannah Lee <013hannah.a.lee@gmail.com>
2021-03-07 07:26:15 -0600

    updated timstamp color js. Still not fully functioning

--- Develop/script.js
+++ Develop/script.js
@@ -2,26 +2,23 @@ var displayDate = document.getElementById("currentDay");
 var todaysDate = moment().format('dddd MMMM DD, YYYY');
 var saveBtn = document.getElementById("save");
 var hours = new Date().getHours();
-var hour = JSON.stringify(hours);
+// var hours = moment(hour).format('h');
 
 displayDate.innerHTML = todaysDate;
-console.log(hours + " hour");
+console.log(hours + "AM");
 
 //changes the color of the planner by the time
 $('.input-group').each(function () {
-    console.log($('.input-group-text', this).text() );
+    console.log($('.input-group-text', this).text());
     var currentTime = $('.input-group-text', this).text();
     var timeStampColor = $('.color', this);
 
-    if (currentTime === hour) {
-        console.log("present");
+    if (currentTime === hours + "AM" || currentTime === hours + "PM") {
         timeStampColor.addClass('present');
-    } else if (currentTime < hour) {
-        console.log("past")
-        timeStampColor.addClass('past');
-    } else {
-        console.log("future")
+    } else if (currentTime < hours + "AM" || currentTime < hours + "PM") {
         timeStampColor.addClass('future');
+    } else {
+        timeStampColor.addClass('past');
     }});
 
    
